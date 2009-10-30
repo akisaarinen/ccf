2fd277d9e60c14c887db307b2080487da5a86a7b
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-10-30 17:57:25 +0200

    Add dependencies to specs, scalacheck and mockito
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

---
+++ project/build/OtProject.scala
@@ -0,0 +1,12 @@
+import sbt._
+import sbt.Configurations._
+
+class OtProject(info: ProjectInfo) extends DefaultProject(info) {
+  override def testFrameworks = ScalaCheckFramework :: SpecsFramework :: Nil
+  override def mainClass = Some("TestMain")
+
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
+  val mockito = "org.mockito" % "mockito-core" % "1.8.0"
+  val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5"
+  override def libraryDependencies = Set(specs, mockito, scalacheck)
+}
