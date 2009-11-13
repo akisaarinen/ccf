2dba1e383cc4ef318ff125c5877650be0edac68d
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-11-13 13:30:21 +0200

    Introduce tree indices for working with tree structures
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

---
+++ src/main/scala/ccf/tree/indexing/TreeIndex.scala
@@ -0,0 +1,56 @@
+package ccf.tree.indexing
+
+case class TreeIndex(val indexPath: Int*) extends Indexable {
+  def shift(count: Int, other: Indexable) = other match {
+    case other: TreeIndex if (other.level > level) => TreeIndex(indexPath: _*)
+    case other: TreeIndex if (other.level == 0) => TreeIndex(indexPath: _*)
+    case other: TreeIndex => {
+      val shiftLevel = other.indexPath.size
+      val shiftedPath =
+        indexPath.toList.take(shiftLevel - 1) ++
+        List(indexPath(shiftLevel-1) + count) ++
+        indexPath.toList.drop(shiftLevel)
+      TreeIndex(shiftedPath: _*)
+    }
+    case _ => TreeIndex(indexPath: _*)
+  }
+
+  def <(other: Indexable) = matchSupported(other) {
+    case UndefinedIndex() => false
+    case other: TreeIndex if (level < other.level) => this < other.parent.shift(1)
+    case other: TreeIndex if (level == other.level) => indexInLevel < other.indexInLevel
+    case other: TreeIndex if (level > other.level) => parent.shift(1) < other
+  }
+
+  def ==(other: Indexable) = matchSupported(other) {
+    case UndefinedIndex() => false
+    case other: TreeIndex => indexPath.toList == other.indexPath.toList // comparison needs to be done as lists
+  }
+
+  def isAffectedBy(other: Indexable) = matchSupported(other) {
+    case other: TreeIndex if (other.level < level) => true
+    case other: TreeIndex if (other.level == level) => parent == other.parent // if parents match
+    case other: TreeIndex if (other.level > level) => false
+    case UndefinedIndex() => false
+  }
+
+  def isDescendantOf(other: Indexable) = matchSupported(other) {
+    case UndefinedIndex() => false
+    case other: TreeIndex if (other.level >= level) =>false
+    case other: TreeIndex if (other.level == level-1) => other == parent
+    case other: TreeIndex if (other.level < level) => parent.isDescendantOf(other)
+  }
+
+  protected def matchSupported(other: Indexable)(pf: PartialFunction[Indexable, Boolean]) = {
+    if (pf isDefinedAt other) pf(other) else throw new UnsupportedOperationException("Unsupported index combination with "+other)
+  }
+
+  def parent = if (level <= 1) TreeIndex() else TreeIndex(indexPath.toList.dropRight(1) : _*)
+  def append(index: Int) = TreeIndex((indexPath.toList ::: List(index)): _*)
+  def append(backlogTreeIndex: TreeIndex) = TreeIndex((indexPath.toList ::: backlogTreeIndex.indexPath.toList): _*)
+
+  def level = indexPath.size
+  def indexInLevel = if (level == 0) 0 else indexPath.last
+  def increment(count: Int) = parent.append(indexInLevel + count)
+  def decrement(count: Int) = parent.append(indexInLevel - count)
+}
---
+++ src/test/scala/ccf/tree/indexing/TreeIndexSpec.scala
@@ -0,0 +1,89 @@
+package ccf.tree.indexing
+
+import org.specs.Specification
+
+object TreeIndexSpec extends Specification {
+  "Any TreeIndex" should {
+    "be equal to itself after initialization" in {
+      TreeIndex(0) mustEqual TreeIndex(0)
+      TreeIndex(0,1) mustEqual TreeIndex(0,1)
+      TreeIndex(0,1,2) mustEqual TreeIndex(0,1,2)
+    }
+    "not be equal to different indices" in {
+      TreeIndex(0) must not equalTo(TreeIndex())
+      TreeIndex(0) must not equalTo(TreeIndex(1))
+      TreeIndex(0) must not equalTo(TreeIndex(0,0))
+    }
+    "be greater than its parent" in {
+      TreeIndex(0,1) > TreeIndex(0) must beTrue
+      TreeIndex(0,1,2) > TreeIndex(0,1) must beTrue
+    }
+    "be greater than previous index in same level" in {
+      TreeIndex(1) > TreeIndex(0) must beTrue
+      TreeIndex(0,2) > TreeIndex(0,1) must beTrue
+    }
+    "construct its parent correctly" in {
+      TreeIndex(1).parent mustEqual TreeIndex()
+      TreeIndex(2,3).parent mustEqual TreeIndex(2)
+      TreeIndex(5,6,7).parent mustEqual TreeIndex(5,6)
+    }
+    "ignore shift when shifted with global parent" in {
+      TreeIndex().shift(1, TreeIndex()) mustEqual TreeIndex()
+      TreeIndex(1).shift(1, TreeIndex()) mustEqual TreeIndex(1)
+      TreeIndex(1,2).shift(1, TreeIndex()) mustEqual TreeIndex(1,2)
+    }
+    "shift first level up when shifted with first level index" in {
+      val firstLevelIndex = TreeIndex(0)
+      TreeIndex(1).shift(1, firstLevelIndex) mustEqual TreeIndex(2)
+      TreeIndex(2,3).shift(1, firstLevelIndex) mustEqual TreeIndex(3,3)
+      TreeIndex(4,5,6).shift(1, firstLevelIndex) mustEqual TreeIndex(5,5,6)
+    }
+    "shift first level down when shifted with first level index" in {
+      val firstLevelIndex = TreeIndex(0)
+      TreeIndex(1).shift(-1, firstLevelIndex) mustEqual TreeIndex(0)
+      TreeIndex(2,3).shift(-1, firstLevelIndex) mustEqual TreeIndex(1,3)
+      TreeIndex(4,5,6).shift(-1, firstLevelIndex) mustEqual TreeIndex(3,5,6)
+    }
+    "shift second level up when shifted with second level index" in {
+      val secondLevelIndex = TreeIndex(0,1)
+      TreeIndex(1).shift(1, secondLevelIndex) mustEqual TreeIndex(1)
+      TreeIndex(2,3).shift(1, secondLevelIndex) mustEqual TreeIndex(2,4)
+      TreeIndex(4,5,6).shift(1, secondLevelIndex) mustEqual TreeIndex(4,6,6)
+    }
+    "shift second level down when shifted with second level index" in {
+      val secondLevelIndex = TreeIndex(0,1)
+      TreeIndex(1).shift(-1, secondLevelIndex) mustEqual TreeIndex(1)
+      TreeIndex(2,3).shift(-1, secondLevelIndex) mustEqual TreeIndex(2,2)
+      TreeIndex(4,5,6).shift(-1, secondLevelIndex) mustEqual TreeIndex(4,4,6)
+    }
+    "be descendent of global parent" in {
+      TreeIndex(1).isDescendantOf(TreeIndex()) must beTrue
+      TreeIndex(1,2).isDescendantOf(TreeIndex()) must beTrue
+    }
+    "be descendent of all of its parents" in {
+      TreeIndex(1,2,3,4).isDescendantOf(TreeIndex()) must beTrue
+      TreeIndex(1,2,3,4).isDescendantOf(TreeIndex(1)) must beTrue
+      TreeIndex(1,2,3,4).isDescendantOf(TreeIndex(1,2)) must beTrue
+      TreeIndex(1,2,3,4).isDescendantOf(TreeIndex(1,2,3)) must beTrue
+    }
+    "not be descendent of items in the same level" in {
+      TreeIndex(1,2).isDescendantOf(TreeIndex(1,1)) must beFalse
+      TreeIndex(1,2).isDescendantOf(TreeIndex(1,2)) must beFalse
+      TreeIndex(1,2).isDescendantOf(TreeIndex(1,3)) must beFalse
+    }
+    "not be descendent of its children" in {
+      TreeIndex(1,2).isDescendantOf(TreeIndex(1,2,1)) must beFalse
+      TreeIndex(1,2).isDescendantOf(TreeIndex(1,2,2)) must beFalse
+    }
+    "be incremented" in {
+      TreeIndex(1).increment(1) must equalTo(TreeIndex(2))
+      TreeIndex(1,2).increment(2) must equalTo(TreeIndex(1,4))
+      TreeIndex(4).increment(-1) must equalTo(TreeIndex(3))
+    }
+    "be decremented" in {
+      TreeIndex(1).decrement(1) must equalTo(TreeIndex(0))
+      TreeIndex(1,2).decrement(2) must equalTo(TreeIndex(1,0))
+      TreeIndex(4).decrement(-1) must equalTo(TreeIndex(5))
+    }
+  }
+}
