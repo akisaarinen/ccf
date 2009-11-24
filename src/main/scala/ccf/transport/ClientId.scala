961b183b639dead0308ba07f0a03bcb4005228e0
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-11-24 17:07:13 +0200

    Introduce a client id class for identifying clients
    This id will be used in the transport layer to identify the clients.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

---
+++ src/main/scala/ccf/transport/ClientId.scala
@@ -0,0 +1,9 @@
+package ccf.transport
+
+import java.util.UUID
+
+case class ClientId(id: UUID)
+
+object ClientId {
+  def randomId = ClientId(UUID.randomUUID)
+}
---
+++ src/test/scala/ccf/transport/ClientIdSpec.scala
@@ -0,0 +1,12 @@
+package ccf.transport
+
+import org.specs.Specification
+
+object ClientIdSpec extends Specification {
+  "Random ClientId" should {
+    val clientId = ClientId.randomId
+    "not be equal to another random ClientId" in {
+      clientId must not equalTo(ClientId.randomId)
+    }
+  }
+}
