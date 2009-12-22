4507982690e3ae31a2e9ced82c47637f4abd4463
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-12-22 16:08:23 +0200

    Introduce a channel shutdown message
    This shall be passed to the clients when channel has been shutdown to
    inform about the situation.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- src/main/scala/ccf/messaging/Message.scala
+++ src/main/scala/ccf/messaging/Message.scala
@@ -4,3 +4,4 @@ import ccf.operation.Operation
 
 case class Message[T <: Operation]()
 case class ConcurrentOperationMessage[T <: Operation](val op: T, val localMessage: Int, val expectedRemoteMessage: Int) extends Message[T]()
+case class ChannelShutdown[T <: Operation](reason: String) extends Message[T]()
