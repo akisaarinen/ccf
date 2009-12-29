b2fb4b86dd4f0c5181930bef66787e0180387223
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-12-29 21:36:12 +0200

    Remove unused import and cleanup printing
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- app/src/main/scala/textapp/TextAppMain.scala
+++ app/src/main/scala/textapp/TextAppMain.scala
@@ -1,12 +1,10 @@
 package textapp
 
-import javax.swing.UIManager
-
 object TextAppMain {
   def main(args: Array[String]) {
     val serverApp = new ServerApp
     val clientApp = new ClientApp
-    println("initialized")
+    println("Text Application started")
   }
 }
 
