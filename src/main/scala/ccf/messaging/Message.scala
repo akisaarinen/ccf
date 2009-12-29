163c0b4fe29041f9255ebbd28e947d6fefffaf36
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-12-29 17:48:40 +0200

    Introduce a generic error message
    In order to deliver custom error messages, we introduce a generic error
    message which can be sent for various purposes. Also make the
    ChannelShutdown extend from ErrorMessage.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- src/main/scala/ccf/messaging/Message.scala
+++ src/main/scala/ccf/messaging/Message.scala
@@ -4,4 +4,5 @@ import ccf.operation.Operation
 
 case class Message[T <: Operation]()
 case class ConcurrentOperationMessage[T <: Operation](val op: T, val localMessage: Int, val expectedRemoteMessage: Int) extends Message[T]()
-case class ChannelShutdown[T <: Operation](reason: String) extends Message[T]()
+case class ErrorMessage[T <: Operation](reason: String) extends Message[T]()
+case class ChannelShutdown[T <: Operation](override val reason: String) extends ErrorMessage[T](reason)
