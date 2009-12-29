f22ba2b67acb59a2b7bf384b8c7862d9ef9a4c46
Aki Saarinen <aki.saarinen@reaktor.fi>
2009-12-29 21:12:16 +0200

    Introduce a parent project and move library code to ccf/
    In order to add also a test application to the same repository, we want
    to keep the library in it's own subdirectory and make a separate project
    for it.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- project/build/CcfProject.scala
+++ project/build/CcfProject.scala
@@ -1,12 +1,17 @@
 import sbt._
 import sbt.Configurations._
 
-class CcfProject(info: ProjectInfo) extends DefaultProject(info) {
-  override def testFrameworks = ScalaCheckFramework :: SpecsFramework :: Nil
-  override def mainClass = Some("TestMain")
+class CcfProject(info: ProjectInfo) extends ParentProject(info) { rootProject =>
+  
+  lazy val lib = project("ccf", "ccf", new CcfLibraryProject(_))
 
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
-  val mockito = "org.mockito" % "mockito-core" % "1.8.0"
-  val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5"
-  override def libraryDependencies = Set(specs, mockito, scalacheck)
+  class CcfLibraryProject(info: ProjectInfo) extends DefaultProject(info) {
+    override def testFrameworks = ScalaCheckFramework :: SpecsFramework :: Nil
+    override def mainClass = Some("TestMain")
+
+    val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
+    val mockito = "org.mockito" % "mockito-core" % "1.8.0"
+    val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5"
+    override def libraryDependencies = Set(specs, mockito, scalacheck)
+  }
 }
