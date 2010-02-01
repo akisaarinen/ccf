4d8aede71b9a87f1354fafaaf5f74b29497e506c
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-02-01 19:52:12 +0200

    app: Extract document handling logic to DocumentHandler
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

---
+++ app/src/main/scala/textapp/server/DocumentHandler.scala
@@ -0,0 +1,51 @@
+package textapp.server
+
+import ccf.messaging.ConcurrentOperationMessage
+import ccf.server.Server
+import ccf.transport.Event
+import ccf.transport.{ClientId, ChannelId}
+import ccf.tree.JupiterTreeTransformation
+import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
+import ccf.{JupiterOperationSynchronizer, JupiterOperationSynchronizerFactory}
+
+class DocumentHandler {
+  private val document = new TextDocument("")
+  private var messages = List[Event.Msg[TreeOperation]]()
+    
+  private val factory = new JupiterOperationSynchronizerFactory(true, JupiterTreeTransformation)
+  private val interceptor = new TextAppOperationInterceptor(document)
+  private val transport = new TextAppTransportActor(onMessageToClient)
+  private val server = new Server[TreeOperation](factory, interceptor, transport)
+
+  private def onMessageToClient(msg: Event.Msg[TreeOperation]): Unit = messages.synchronized {
+    messages = messages ::: List(msg)
+  }
+
+  def onJoin(clientId: ClientId, channelId: ChannelId): TextDocument = {
+    server !? Event.Join(clientId, channelId) match {
+      case Event.State(_, _, state) => state.asInstanceOf[TextDocument]
+      case _ => error("Join failed")
+    }
+  }
+
+  def onQuit(clientId: ClientId, channelId: ChannelId) {
+    server !? Event.Quit(clientId, channelId)
+  }
+  
+  def onMsg(clientId: ClientId, channelId: ChannelId, msg: ConcurrentOperationMessage[TreeOperation]) {
+    server !? Event.Msg(clientId, channelId, msg)
+  }
+
+  def getMessages(clientId: ClientId): (List[ConcurrentOperationMessage[TreeOperation]], TextDocument) = messages.synchronized {
+    def isForClient(msg: Event.Msg[_]) = msg match {
+      case Event.Msg(id, _, _) if (id == clientId) => true
+      case _ => false
+    }
+
+    val msgsForClient = messages.filter(isForClient(_))
+    val msgsNotForClient = messages.filter(!isForClient(_))
+    messages = msgsNotForClient
+    val msgsAsCom = msgsForClient.map(_.msg.asInstanceOf[ConcurrentOperationMessage[TreeOperation]])
+    (msgsAsCom, document)
+  }
+}
--- app/src/main/scala/textapp/server/TextAppRequestHandler.scala
+++ app/src/main/scala/textapp/server/TextAppRequestHandler.scala
@@ -1,13 +1,7 @@
 package textapp.server
 
 import MessageCoding.{encode, decode}
-import ccf.{JupiterOperationSynchronizer, JupiterOperationSynchronizerFactory}
-import ccf.messaging.ConcurrentOperationMessage
 import ccf.transport.{ClientId, ChannelId}
-import ccf.tree.JupiterTreeTransformation
-import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
-import ccf.transport.{Event, TransportActor}
-import ccf.server.Server
 import com.sun.net.httpserver.{HttpHandler, HttpExchange}
 import java.net.URI
 import java.util.UUID
@@ -18,8 +12,6 @@ class TextAppRequestHandler extends HttpHandler {
   import net.liftweb.json.JsonAST
   import net.liftweb.json.JsonAST._
   import net.liftweb.json.JsonDSL._
-
-  private val document = new TextDocument("")
   
   private val page404 = 
     <html>
@@ -37,13 +29,8 @@ class TextAppRequestHandler extends HttpHandler {
       </body>
     </html>
 
-  private var messages = List[Event.Msg[TreeOperation]]()
+  private val documentHandler = new DocumentHandler
   private val defaultChannel = ChannelId.randomId
-    
-  private val factory = new JupiterOperationSynchronizerFactory(true, JupiterTreeTransformation)
-  private val interceptor = new TextAppOperationInterceptor(document)
-  private val transport = new TextAppTransportActor(onMessageToClient)
-  private val server = new Server[TreeOperation](factory, interceptor, transport)
 
   def handle(exchange: HttpExchange) {
     try {
@@ -82,42 +69,27 @@ class TextAppRequestHandler extends HttpHandler {
 
     val json: JsonAST.JValue = uri.toString match {
       case JoinExpr() =>
-        server !? Event.Join(id, defaultChannel)
+        val document = documentHandler.onJoin(id, defaultChannel)
         ("status" -> "ok") ~
         ("document" -> document.text)
       case QuitExpr() =>
-        server !? Event.Quit(id, defaultChannel)
+        documentHandler.onQuit(id, defaultChannel)
         ("status" -> "ok")
       case AddExpr() => 
         val encodedMsg = params("msg")
         val msg = decode(encodedMsg)
-        server !? Event.Msg(id, defaultChannel, msg)
+        documentHandler.onMsg(id, defaultChannel, msg)
         ("status" -> "ok")
       case GetExpr() => 
-        val msgs = getMsgsForClient(id).map(encode(_))
+        val (msgs, document) = documentHandler.getMessages(id)
+        val encodedMsgs = msgs.map(encode(_))
         ("status" -> "ok") ~
-        ("msgs" -> msgs) ~
+        ("msgs" -> encodedMsgs) ~
         ("hash" -> document.hash)
       case _ => 
         ("status" -> "error") ~
         ("error" -> "unknown uri")
     }
     Some(compact(JsonAST.render(json)))
-  }
-
-  private def onMessageToClient(msg: Event.Msg[TreeOperation]): Unit = messages.synchronized {
-    messages = messages ::: List(msg)
-  }
-
-  private def getMsgsForClient(id: ClientId): List[ConcurrentOperationMessage[TreeOperation]] = messages.synchronized {
-    def isForClient(msg: Event.Msg[_]) = msg match {
-      case Event.Msg(clientId, _, _) if (id == clientId) => true
-      case _ => false
-    }
-
-    val msgsForClient = messages.filter(isForClient(_))
-    val msgsNotForClient = messages.filter(!isForClient(_))
-    messages = msgsNotForClient
-    msgsForClient.map(_.msg.asInstanceOf[ConcurrentOperationMessage[TreeOperation]])
   }
 }
