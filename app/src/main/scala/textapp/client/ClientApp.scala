e697346288fd53684365588a9776d62df0bb5404
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-02-07 16:53:43 +0200

    app: Extract logic to MainFrame
    Move logic from ClientApp to a different frame. In preparation for
    structuring the code in client side test application a little bit
    better.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- app/src/main/scala/textapp/client/ClientApp.scala
+++ app/src/main/scala/textapp/client/ClientApp.scala
@@ -1,30 +1,19 @@
 package textapp.client
 
 import ccf.JupiterOperationSynchronizer
-import ccf.messaging.ConcurrentOperationMessage
 import ccf.tree.JupiterTreeTransformation
-import ccf.tree.indexing.TreeIndex
 import ccf.transport.ClientId
-import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
+import ccf.tree.operation.TreeOperation
 import java.util.{Timer, TimerTask}
-import javax.swing.JFrame
 import MessageCoding.encode
 
 class ClientApp {
   private val clientSync = new JupiterOperationSynchronizer[TreeOperation](false, JupiterTreeTransformation)
   
-  val frame = new JFrame("TextApp")
   val httpClient = new HttpClient(ClientId.randomId)
-
   val initialText = httpClient.join
   val document = new TextDocument(initialText)
-  val textArea = new TextArea(initialText, onInsert, onDelete)
-
-  frame.getContentPane().add(textArea)
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-  frame.pack()
-  frame.setVisible(true)
-
+  val frame = new MainFrame(document, sendToServer)
   val timer = new Timer
   timer.scheduleAtFixedRate(syncTask, 0, 500)
 
@@ -43,20 +32,8 @@ class ClientApp {
   }
 
   private def applyOperationLocally(op: TreeOperation) {
-      textArea.applyOp(op)
-      document.applyOp(op)
-  }
-
-  private def onInsert(items: List[(Int, Char)]) {
-    items.foreach { case (i, c) => 
-      sendToServer(InsertOperation(TreeIndex(i), Elem(c)))
-    }
-  }
-
-  private def onDelete(items: List[Int]) {
-    items.foreach { i =>
-      sendToServer(DeleteOperation(TreeIndex(i)))
-    }
+    document.applyOp(op)
+    frame.applyOp(op)
   }
   
   private def sendToServer(op: TreeOperation) {
---
+++ app/src/main/scala/textapp/client/MainFrame.scala
@@ -0,0 +1,29 @@
+package textapp.client
+
+import ccf.tree.indexing.TreeIndex
+import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
+import javax.swing.JFrame
+
+class MainFrame(document: TextDocument, sendToServer: TreeOperation => Unit) extends JFrame("libccf test application") {
+  val textArea = new TextArea(document.text, onInsert, onDelete)
+  getContentPane().add(textArea)
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  pack()
+  setVisible(true)
+
+  def applyOp(op: TreeOperation) {
+    textArea.applyOp(op)
+  }
+
+  private def onInsert(items: List[(Int, Char)]) {
+    items.foreach { case (i, c) => 
+      sendToServer(InsertOperation(TreeIndex(i), Elem(c)))
+    }
+  }
+
+  private def onDelete(items: List[Int]) {
+    items.foreach { i =>
+      sendToServer(DeleteOperation(TreeIndex(i)))
+    }
+  }
+}
