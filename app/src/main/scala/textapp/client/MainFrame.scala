67fa73fba1de0dd16cdaf026817b9e857ff74607
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-02-07 20:37:26 +0200

    app: Add info texts about coloring to the GUI
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- app/src/main/scala/textapp/client/MainFrame.scala
+++ app/src/main/scala/textapp/client/MainFrame.scala
@@ -2,9 +2,10 @@ package textapp.client
 
 import ccf.tree.indexing.TreeIndex
 import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
-import javax.swing.JFrame
 import textapp.client.jgoodies.FormsPanel
 import textapp.{TextDocument, Elem}
+import javax.swing.{JLabel, JFrame}
+import java.awt.Color
 
 class MainFrame(document: TextDocument, sendToServer: TreeOperation => Unit) extends JFrame("libccf test application") {
   val textArea = new TextArea(document.text, onInsert, onDelete)
@@ -27,7 +28,19 @@ class MainFrame(document: TextDocument, sendToServer: TreeOperation => Unit) ext
     }
   }
 
-  class MainPanel(textArea: TextArea) extends FormsPanel("pref", "pref") {
-    add(textArea, xy(1,1))
+  class MainPanel(textArea: TextArea) extends FormsPanel("pref", "pref, pref") {
+    val infoPanel = new FormsPanel("pref", "20px, 14px, 14px, 8px") {
+      val genericInfo = new JLabel("Collaborative text-editing using CCF")
+      val ownTextInfo = new JLabel("* Own text is shown with green color")
+      val otherTextInfo = new JLabel("* Others' text is shown with red color")
+      ownTextInfo.setForeground(new Color(0, 150, 0))
+      otherTextInfo.setForeground(new Color(255, 0, 0))
+
+      add(genericInfo, xy(1,1))
+      add(ownTextInfo, xy(1,2))
+      add(otherTextInfo, xy(1,3))
+    }
+    add(infoPanel, xy(1,1))
+    add(textArea, xy(1,2))
   }
 }
