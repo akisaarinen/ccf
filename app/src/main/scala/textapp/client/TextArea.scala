c8b09f581a1305bb2ff14e6b38dd11d7189a4e9a
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-02-07 20:01:12 +0200

    app: Color local text green and remote text red
    Use green color for text that has been inputted locally and red color
    for all remote edits. Gives a nicer visual indication of the
    collaborativeness of the application. JTextArea has been changed to
    JTextPane so we can use the styled document attributes to color the text
    with different color in different parts. Input text is colored by
    overriding #getInputAttributes (the only working way I could find to
    reliably make input color green).
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- app/src/main/scala/textapp/client/TextArea.scala
+++ app/src/main/scala/textapp/client/TextArea.scala
@@ -2,25 +2,37 @@ package textapp.client
 
 import ccf.tree.indexing.TreeIndex
 import ccf.tree.operation.{TreeOperation, InsertOperation, DeleteOperation}
-import java.awt.Dimension
-import javax.swing.JTextArea
-import javax.swing.text.SimpleAttributeSet
 import textapp.Elem
+import java.awt.{Color, Dimension}
+import javax.swing.JTextPane
+import javax.swing.text.{StyleConstants, SimpleAttributeSet, StyleContext}
 
-class TextArea(initialText: String, 
+class TextArea(initialText: String,
                onInsert: List[(Int, Char)] => Unit, 
-               onDelete: List[Int] => Unit) extends JTextArea {
+               onDelete: List[Int] => Unit) extends JTextPane {
+  private val defaultStyleContext = StyleContext.getDefaultStyleContext
+  private val remoteEditColor = new Color(255, 0, 0)
+  private val remoteEditAttributes = defaultStyleContext.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, remoteEditColor);
+  private val localEditColor = new Color(0, 150, 0)
+  private val localEditAttributes = defaultStyleContext.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, localEditColor);
+
   setPreferredSize(new Dimension(600, 400))
   setText(initialText)
   val listener = new TextComponentDocumentListener(onInsert, onDelete)
   getDocument.addDocumentListener(listener)
 
+  override def getInputAttributes = {
+    val originalInputAttributes = super.getInputAttributes
+    StyleConstants.setForeground(originalInputAttributes, localEditColor)
+    originalInputAttributes
+  }
+
   def applyOp(op: TreeOperation) {
     getDocument.removeDocumentListener(listener)
     op match {
       case InsertOperation(TreeIndex(i), Elem(c)) =>         
-        getDocument.insertString(i, c.toString, SimpleAttributeSet.EMPTY)
-      case DeleteOperation(TreeIndex(i)) => 
+        getDocument.insertString(i, c.toString, remoteEditAttributes)
+      case DeleteOperation(TreeIndex(i)) =>
         getDocument.remove(i, 1)
       case _ => error("unable to apply operation to text area ('%s')".format(op))
     }
