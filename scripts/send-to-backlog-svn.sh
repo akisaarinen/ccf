d935f41e0a2f492357bef863fc62a4bfa183b283
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-03-01 19:47:55 +0200

    Update default smtp server address
    Preferred smtp server address is smtp.core.reaktor.fi, which is also
    open through Reaktor VPN connections, so we'll use that instead of
    mx.reaktor.fi.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- scripts/send-to-backlog-svn.sh
+++ scripts/send-to-backlog-svn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SMTP_SERVER=mx.reaktor.fi
+SMTP_SERVER=smtp.core.reaktor.fi
 SMTP_SERVER_PORT=25
 MAIL_ADDR=backlog-git@reaktor.fi
 git send-email --suppress-from --smtp-server=$SMTP_SERVER --smtp-server-port=$SMTP_SERVER_PORT --to=$MAIL_ADDR $1
