95f24df0879f67695fcb0835ef2c89c6af768b7f
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-03-02 21:14:01 +0200

    lib: Improve unit tests of JupiterTreeTransformation
    Add various corner-case combinations which validate that the
    transformation of pairwise operations works as expected with
    JupiterTreeTransformation.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- ccf/src/test/scala/ccf/tree/JupiterTreeTransformationSpec.scala
+++ ccf/src/test/scala/ccf/tree/JupiterTreeTransformationSpec.scala
@@ -13,32 +13,174 @@ object JupiterTreeTransformationSpec extends Specification {
     }
   }
 
-  "JupiterTreeTransformation with local move and remote update" should {
-    "not affect update if move is in front of the update" in {
+  "JupiterTreeTransformation with local insert in front of remote operation" should {
+    val localOp = insert(1, "A")
+    "move remote insert forward" in {
+      val remoteOp = insert(2, "B")
+      transformBoth(localOp, remoteOp) must equalTo(insert(3, "B"))
+    }
+    "move remote delete forward" in {
+      val remoteOp = delete(2)
+      transformBoth(localOp, remoteOp) must equalTo(delete(3))
+    }
+    "move remote update forward" in {
+      val remoteOp = update(2, "B")
+      transformBoth(localOp, remoteOp) must equalTo(update(3, "B"))
+    }
+    "move remote target index of move forward" in {
+      val remoteOp = move(0, 7)
+      transformBoth(localOp, remoteOp) must equalTo(move(0, 8))
+    }
+    "move source index of remote move forward" in {
+      val remoteOp = move(5, 0)
+      transformBoth(localOp, remoteOp) must equalTo(move(6, 0))
+    }
+    "move source and target index of remote move forward" in {
+      val remoteOp = move(5, 7)
+      transformBoth(localOp, remoteOp) must equalTo(move(6, 8))
+    }
+  }
+
+  "JupiterTreeTransformation with local delete in front of remote operation" should {
+    val localOp = delete(1)
+    "move remote insert backward" in {
+      val remoteOp = insert(2, "B")
+      transformBoth(localOp, remoteOp) must equalTo(insert(1, "B"))
+    }
+    "move remote delete backward" in {
+      val remoteOp = delete(2)
+      transformBoth(localOp, remoteOp) must equalTo(delete(1))
+    }
+    "move remote update backward" in {
+      val remoteOp = update(2, "B")
+      transformBoth(localOp, remoteOp) must equalTo(update(1, "B"))
+    }
+    "move target index of remote move backward" in {
+      val remoteOp = move(0, 7)
+      transformBoth(localOp, remoteOp) must equalTo(move(0, 6))
+    }
+    "move source index of remote move backward" in {
+      val remoteOp = move(5, 0)
+      transformBoth(localOp, remoteOp) must equalTo(move(4, 0))
+    }
+    "move source and target index of remote move backward" in {
+      val remoteOp = move(5, 7)
+      transformBoth(localOp, remoteOp) must equalTo(move(4, 6))
+    }
+  }
+
+  "JupiterTreeTransformation with local delete that invalidates remote operations" should {
+    val localOp = delete(1)
+    "not invalidate insert to same index" in {
+      val remoteOp = insert(1, "B")
+      transformBoth(localOp, remoteOp) must equalTo(remoteOp)
+    }
+    "not invalidate insert to index after delete" in {
+      val remoteOp = insert(2, "B")
+      transformBoth(localOp, remoteOp) must equalTo(insert(1, "B"))
+    }
+    "invalidate remote delete to same index" in {
+      val remoteOp = delete(1)
+      transformBoth(localOp, remoteOp) must equalTo(NoOperation())
+    }
+    "invalidate remote update to same index" in {
+      val remoteOp = update(1, "B")
+      transformBoth(localOp, remoteOp) must equalTo(NoOperation())
+    }
+    "invalidate remote move from same source index to forward" in {
+      val remoteOp = move(1, 9)
+      transformBoth(localOp, remoteOp) must equalTo(NoOperation())
+    }
+    "invalidate remote move from same source index to backward" in {
+      val remoteOp = move(1, 0)
+      transformBoth(localOp, remoteOp) must equalTo(NoOperation())
+    }
+  }
+
+  "JupiterTreeTransformation with local move and remote operations to other nodes" should {
+    "not affect remote update if move is in front of the update" in {
       val localOp = move(0, 1)
       val remoteOp = update(2, "A")
       transformBoth(localOp, remoteOp) must equalTo(remoteOp)
     }
 
-    "not affect update if move is after the update" in {
+    "not affect remote update if move is after the update" in {
       val localOp = move(3, 4)
       val remoteOp = update(2, "A")
       transformBoth(localOp, remoteOp) must equalTo(remoteOp)
     }
+  }
 
-    "track update when updating node has been moved forward" in {
+  "JupiterTreeTransformation with local move and remote operations targeting the moved node" should {
+    "track remote update when node has been moved forward" in {
       val localOp = move(2, 8)
       val remoteOp = update(2, "A")
       transformBoth(localOp, remoteOp) must equalTo(update(8, "A"))
     }
 
-    "track update when updating node has been moved backward" in {
+    "track remote update when node has been moved backward" in {
       val localOp = move(2, 0)
       val remoteOp = update(2, "A")
       transformBoth(localOp, remoteOp) must equalTo(update(0, "A"))
     }
+
+    "track remote update when node has been moved in-place" in {
+      val localOp = move(2, 2)
+      val remoteOp = update(2, "A")
+      transformBoth(localOp, remoteOp) must equalTo(update(2, "A"))
+    }
+
+    "track remote delete when node has been moved forward" in {
+      val localOp = move(2, 8)
+      val remoteOp = delete(2)
+      transformBoth(localOp, remoteOp) must equalTo(delete(8))
+    }
+
+    "track remote delete when node has been moved backward" in {
+      val localOp = move(2, 0)
+      val remoteOp = delete(2)
+      transformBoth(localOp, remoteOp) must equalTo(delete(0))
+    }
+
+    "track remote delete when node has been moved in-place" in {
+      val localOp = move(2, 2)
+      val remoteOp = delete(2)
+      transformBoth(localOp, remoteOp) must equalTo(delete(2))
+    }
+
+    "track remote move when node has been moved forward and remote is primary" in {
+      val localOp = move(2, 8)
+      val remoteOp = move(2, 4)
+      transformSecondary(localOp, remoteOp) must equalTo(move(8, 4))
+    }
+
+    "track remote move when node has been moved backward and remote is primary" in {
+      val localOp = move(2, 0)
+      val remoteOp = move(2, 4)
+      transformSecondary(localOp, remoteOp) must equalTo(move(0, 4))
+    }
+
+    "track remote move when node has been moved in-place" in {
+      val localOp = move(2, 2)
+      val remoteOp = move(2, 4)
+      transformBoth(localOp, remoteOp) must equalTo(move(2, 4))
+    }
+
+    "invalidate remote move when node has been moved forward and local is primary" in {
+      val localOp = move(2, 8)
+      val remoteOp = move(2, 4)
+      transformPrimary(localOp, remoteOp) must equalTo(NoOperation())
+    }
+
+    "invalidate remote move when node has been moved backward and local is primary" in {
+      val localOp = move(2, 0)
+      val remoteOp = move(2, 4)
+      transformPrimary(localOp, remoteOp) must equalTo(NoOperation())
+    }
   }
 
+  private def insert(i: Int, name: String) = new InsertOperation(TreeIndex(i), Node(name))
+  private def delete(i: Int) = new DeleteOperation(TreeIndex(i))
   private def update(i: Int, name: String) = new UpdateAttributeOperation(TreeIndex(i), name, NopModifier())
   private def move(src: Int, dst: Int) = new MoveOperation(TreeIndex(src), TreeIndex(dst))
 
@@ -56,4 +198,6 @@ object JupiterTreeTransformationSpec extends Specification {
     asPrimary must equalTo(asSecondary)
     asPrimary
   }
+
+  private case class Node(name: String) extends TreeNode
 }
