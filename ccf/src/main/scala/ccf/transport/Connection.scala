48e3f21f5dcd6de0d7f26d110838e68affe380fc
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2010-05-03 16:47:26 +0300

    transport: Declare #send to throw an InvalidRequestException
    Signed-off-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/transport/Connection.scala
+++ ccf/src/main/scala/ccf/transport/Connection.scala
@@ -3,6 +3,7 @@ package ccf.transport
 trait Connection {
   @throws(classOf[ConnectionException])
   def invoke(method: String, args: String): String
+  @throws(classOf[InvalidRequestException])
   @throws(classOf[MalformedDataException])
   @throws(classOf[ConnectionException])
   def send(request: Request): Option[Response]
