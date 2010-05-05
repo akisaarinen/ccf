dfe7d69b597c4387cfbe17528cb5ba6e4c9a16cc
Karim Osman <karim.osman@reaktor.fi>
2010-05-05 11:38:05 +0300

    transport: Drop direct dep. from HTTP connection to Databinder Dispatch
    While at it, transform IO exceptions to ConnectionExceptions.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

--- ccf/src/main/scala/ccf/transport/http/HttpConnection.scala
+++ ccf/src/main/scala/ccf/transport/http/HttpConnection.scala
@@ -3,26 +3,17 @@ package ccf.transport.http
 import java.io.IOException
 import java.net.URL
 
-import org.apache.http.params.HttpConnectionParams
 import ccf.transport.json.{JsonFormatter, JsonParser}
-
-import dispatch.{Request => HttpRequest, Http}
-import dispatch.Http._
 
 class HttpConnection(url: URL, timeoutMillis: Int, http: Http) extends Connection {
   private val formatter = JsonFormatter
   private val parser = JsonParser
-  init
-  def this(url: URL, timeoutMillis: Int) = this(url, timeoutMillis, new Http)
-  def send(request: Request): Option[Response] = {
-    val req = requestUrl(request).POST << formatter.format(request)
-    http(req >- { parser.parse(_) })
+  def this(url: URL, timeoutMillis: Int) = this(url, timeoutMillis, new HttpImpl(timeoutMillis))
+  def send(request: Request): Option[Response] = try {
+    http.post(requestUrl(request), formatter.format(request)) { parser.parse }
+  } catch {
+    case e: IOException => throw new ConnectionException(e.toString)
   }
-  private def requestUrl(request: Request) = url.toString / request.header("type").getOrElse(requestTypeMissing)
+  private def requestUrl(request: Request) = new URL(url, request.header("type").getOrElse(requestTypeMissing))
   private def requestTypeMissing = throw new InvalidRequestException("Request header \"type\" missing")
-  private def init = {
-    HttpConnectionParams.setConnectionTimeout(httpClientParams, timeoutMillis)
-    HttpConnectionParams.setSoTimeout(httpClientParams, timeoutMillis)
-  }
-  private def httpClientParams = http.client.getParams
 }
