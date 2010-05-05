7c3ff0c4579e2c035d10aa0d76cee21d3c65d64a
Karim Osman <karim.osman@reaktor.fi>
2010-05-05 11:34:40 +0300

    transport: Add HTTP access point impl using Databinder Dispatch
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

---
+++ ccf/src/main/scala/ccf/transport/http/HttpImpl.scala
@@ -0,0 +1,19 @@
+package ccf.transport.http
+
+import org.apache.http.params.HttpConnectionParams
+
+import dispatch.{Http => DispatchHttp}
+import dispatch.Http._
+
+import java.net.URL
+
+class HttpImpl(timeoutMillis: Int) extends Http {
+  private val http = new DispatchHttp
+  init
+  def post[T](url: URL, data: String)(block: String => T): T = http(url.toString.POST << data >- { block })
+  private def init {
+    HttpConnectionParams.setConnectionTimeout(httpClientParams, timeoutMillis)
+    HttpConnectionParams.setSoTimeout(httpClientParams, timeoutMillis)
+  }
+  private def httpClientParams = http.client.getParams
+}
