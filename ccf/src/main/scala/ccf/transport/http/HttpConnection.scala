a1b592ec545120fdadc0cf8fc77d6e753fd8ce61
Karim Osman <karim.osman@reaktor.fi>
2010-05-05 12:21:13 +0300

    transport: Update overloaded ctor to receive Parser and Formatter
    This allows passing of mocked Parser and Formatter.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

--- ccf/src/main/scala/ccf/transport/http/HttpConnection.scala
+++ ccf/src/main/scala/ccf/transport/http/HttpConnection.scala
@@ -5,10 +5,8 @@ import java.net.URL
 
 import ccf.transport.json.{JsonFormatter, JsonParser}
 
-class HttpConnection(url: URL, timeoutMillis: Int, http: Http) extends Connection {
-  private val formatter = JsonFormatter
-  private val parser = JsonParser
-  def this(url: URL, timeoutMillis: Int) = this(url, timeoutMillis, new HttpImpl(timeoutMillis))
+class HttpConnection(url: URL, timeoutMillis: Int, http: Http, parser: Parser, formatter: Formatter) extends Connection {
+  def this(url: URL, timeoutMillis: Int) = this(url, timeoutMillis, new HttpImpl(timeoutMillis), JsonParser, JsonFormatter)
   def send(request: Request): Option[Response] = try {
     http.post(requestUrl(request), formatter.format(request)) { parser.parse }
   } catch {
