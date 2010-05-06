683f5deb0cf7ac582a0ebc83c9221519b2cb9c5d
Karim Osman <karim.osman@reaktor.fi>
2010-05-06 15:31:56 +0300

    perftest: Add support for running server from Perftest
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

--- perftest/src/main/scala/perftest/Perftest.scala
+++ perftest/src/main/scala/perftest/Perftest.scala
@@ -1,6 +1,7 @@
 package perftest
 
 import perftest.client.Client
+import perftest.server.Server
 
 import java.net.URL
 
@@ -8,7 +9,7 @@ object Perftest {
   private def usage = println("Usage: Perftest [client|server] URL")
   private def parse(args: Array[String]) = if (args.length > 1) Some(args(0), new URL(args(1))) else None
   def main(args: Array[String]) = parse(args) match {
-    case Some(("server", url)) => error("server not yet supported")
+    case Some(("server", url)) => Server.run(url)
     case Some(("client", url)) => Client.main(Array(url.toString))
     case None                  => usage
   }
