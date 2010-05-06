cac343e49cc0be50338a4c80e1a940ef65abe802
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2010-05-06 11:11:02 +0300

    transport: Add Response formatting support to Formatter
    Introduce #formatResponse to Formatter along with a stub implementation
    in JsonFormatter.
    
    Signed-off-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/transport/Formatter.scala
+++ ccf/src/main/scala/ccf/transport/Formatter.scala
@@ -2,4 +2,5 @@ package ccf.transport
 
 trait Formatter {
   def formatRequest(request: Request): String
+  def formatResponse(response: Response): String
 }
--- ccf/src/main/scala/ccf/transport/json/JsonFormatter.scala
+++ ccf/src/main/scala/ccf/transport/json/JsonFormatter.scala
@@ -4,6 +4,7 @@ import com.twitter.json.Json
 
 object JsonFormatter extends Formatter {
   def formatRequest(request: Request): String = Json.build(toMap(request)).toString
+  def formatResponse(response: Response): String = error("Not implemented")
   private def toMap(request: Request) = request.content match {
     case Some(c) => Map("headers" -> request.headers, "content" -> c)
     case None    => Map("headers" -> request.headers)
