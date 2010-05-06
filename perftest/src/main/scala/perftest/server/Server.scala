56e413265e459ea62f9bb24a733e5ebae73f1d1e
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2010-05-06 11:30:02 +0300

    perftest: Use JsonFormatter#formatResponse in Server
    Server uses JsonFormatter#formatRequest to generate response. This was a
    workaround due to lack of Formatter#formatResponse.
    
    Since Formatter now provides #formatResponse, the hack can be removed.
    
    Signed-off-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- perftest/src/main/scala/perftest/server/Server.scala
+++ perftest/src/main/scala/perftest/server/Server.scala
@@ -4,14 +4,14 @@ import com.sun.net.httpserver.HttpServer
 import com.sun.net.httpserver.{HttpHandler, HttpExchange}
 import java.net.InetSocketAddress
 
-import ccf.transport.Request
+import ccf.transport.Response
 import ccf.transport.json.JsonFormatter
 
 class HttpRequestHandler extends HttpHandler {
   def handle(exchange: HttpExchange) { 
     try {
-      val response = Request(Map[String, String](), Some((0 to 1023).map(x => 0).mkString("")))
-      val body = JsonFormatter.formatRequest(response)
+      val response = Response(Map[String, String](), Some((0 to 1023).map(x => 0).mkString("")))
+      val body = JsonFormatter.formatResponse(response)
       exchange.sendResponseHeaders(200, body.length)
       exchange.getResponseBody.write(body.getBytes)
     } finally {
