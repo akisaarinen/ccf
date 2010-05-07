072518dee4da93f18101bc2dae94ae272b4dacf6
Karim Osman <karim.osman@reaktor.fi>
2010-05-07 16:40:44 +0300

    transport: Test that #send throws a ConnectionException upon IOException
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

--- ccf/src/test/scala/ccf/transport/http/HttpConnectionSpec.scala
+++ ccf/src/test/scala/ccf/transport/http/HttpConnectionSpec.scala
@@ -3,6 +3,7 @@ package ccf.transport.http
 import org.specs.Specification
 import org.specs.mock.Mockito
 
+import java.io.IOException
 import java.net.URL
 
 object HttpConnectionSpec extends Specification with Mockito {
@@ -14,6 +15,21 @@ object HttpConnectionSpec extends Specification with Mockito {
       val formatter = mock[Formatter]
       val conn = new HttpConnection(url, client, parser, formatter) 
       conn.send(new Request(Map[String, String](), None)) must throwA[InvalidRequestException]
+    }
+  }
+  "IOException thrown from HttpClient#post" should {
+    val requestData = "data"
+    val spec = "spec"
+    val url = new URL("http://www.url")
+    "cause a ConnectionException" in {
+      val request = mock[Request]
+      request.header("type") returns Some(spec)
+      val formatter = mock[Formatter]
+      formatter.formatRequest(request) returns requestData
+      val client = mock[HttpClient]
+      client.post(new URL(url, spec), requestData) throws new IOException
+      val conn = new HttpConnection(url, client, mock[Parser], formatter)
+      conn.send(request) must throwA[ConnectionException]
     }
   }
 }
