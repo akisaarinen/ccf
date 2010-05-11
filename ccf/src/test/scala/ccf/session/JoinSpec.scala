5bf1b657627cde9e257265d14541c1cd6ac6472c
Karim Osman <karim.osman@reaktor.fi>
2010-05-11 14:30:03 +0300

    session: Leave optional response unbound when invoking #send in JoinSpec
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>

--- ccf/src/test/scala/ccf/session/JoinSpec.scala
+++ ccf/src/test/scala/ccf/session/JoinSpec.scala
@@ -2,7 +2,7 @@ package ccf.session
 
 import org.specs.Specification
 import org.specs.mock.Mockito
-import ccf.transport.{Connection, Response}
+import ccf.transport.Connection
 
 object JoinSpec extends Specification with Mockito {
   val connection = mock[Connection]
@@ -15,15 +15,15 @@ object JoinSpec extends Specification with Mockito {
     val joinRequest = JoinRequest(newChannelId)(session)
     val joinMessage = Join(newChannelId)
     connection.send(joinRequest) returns None
-    "invoke #send with a valid join request and return a valid new session on #send" in {
-      val (nextSession: Session, r: Option[Response]) = joinMessage.send(session)
+    "invoke #send with a message and return a valid new session" in {
+      val (nextSession: Session, _) = joinMessage.send(session)
       nextSession.seqId must be equalTo(1)
       nextSession.channels must be equalTo(Set(existingChannelId, newChannelId))
       connection.send(joinRequest) was called
     }
     "take no action if already a member of a channel" in {
       val joinExistingMessage = Join(existingChannelId)
-      val (nextSession: Session, r: Option[Response]) = joinExistingMessage.send(session)
+      val (nextSession: Session, _) = joinExistingMessage.send(session)
       nextSession must be equalTo(session)
       connection.send(joinRequest) wasnt called
     }
