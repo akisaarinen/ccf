38d4dd5081efc77d4d5a59815707f75296f81502
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2010-05-11 13:05:03 +0300

    session: Implement #send to Session
    Session is responsible for sending message and checking (optional)
    response sequence ids in order to detect inconsistencies in the
    request-resposen message exchange pattern.
    
    Current implementation does not yet process responses.
    
    Signed-off-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/session/Session.scala
+++ ccf/src/main/scala/ccf/session/Session.scala
@@ -4,4 +4,8 @@ import ccf.transport.{Connection, Request, Response, ConnectionException}
 
 case class Session(connection: Connection, version: Version, clientId: ClientId, seqId: Int, channels: Set[ChannelId]) {
   def next(channels: Set[ChannelId]) = Session(connection, version, clientId, seqId + 1, channels)
+  def send(msg: Message): Session = msg.send(this) match {
+    case (session: Session, Some(response)) => session
+    case (session: Session, None)           => session
+  }
 }
--- ccf/src/test/scala/ccf/session/SessionSpec.scala
+++ ccf/src/test/scala/ccf/session/SessionSpec.scala
@@ -5,16 +5,41 @@ import org.specs.mock.Mockito
 import ccf.transport.Connection
 
 object SessionSpec extends Specification with Mockito {
+  val connection = mock[Connection]
+  val version = Version(1, 2)
+  val clientId = ClientId.randomId
   val existingChannelId = ChannelId.randomId
   val newChannelId = ChannelId.randomId
   val channels = Set(existingChannelId)
-  val session = Session(mock[Connection], mock[Version], mock[ClientId], 0, channels)
+  val session = Session(connection, version, clientId, 0, channels)
   "Session next(...)" should {
     "produce new session with incremented seqId" in {
       session.next(Set()).seqId must equalTo(1)
     }
     "produce new session with updated channel list" in {
       session.next(Set(newChannelId)).channels must equalTo(Set(newChannelId))
+    }
+  }
+  "Session send(...)" should {
+    "send valid Join message, sending correct Request and producing correct Session" in {
+      val joinRequest = JoinRequest(newChannelId)(session)
+      val joinMessage = Join(newChannelId)
+      connection.send(joinRequest) returns None
+      val nextSession = session.send(joinMessage)
+
+      nextSession.seqId must equalTo(1)
+      nextSession.channels must equalTo(Set(existingChannelId, newChannelId))
+      connection.send(joinRequest) was called
+    }
+    "send Part message, sending correct Request and producing correct Session" in {
+      val partRequest = PartRequest(existingChannelId)(session)
+      val partMessage = Part(existingChannelId)
+      connection.send(partRequest) returns None
+      val nextSession = session.send(partMessage)
+
+      nextSession.seqId must equalTo(1)
+      nextSession.channels must equalTo(Set())
+      connection.send(partRequest) was called
     }
   }
 }
