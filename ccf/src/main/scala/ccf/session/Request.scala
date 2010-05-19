32292afc6c5da609f09fc69e2e7cbe3393cf7e35
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2010-05-19 14:39:18 +0300

    session: Add OperationRequest
    OperationRequest constructs Request object for a given request type and
    content. The content is handled as opaque data. OperationRequest is used
    to transmit the actual application layer payload data.
    
    However, the content must only contain base types, otherwise formatters
    and parsers may fail handling it.
    
    While at it, reindent the Map(...) construction to fit into 80 columns.
    
    Signed-off-by: Harri Salokorpi <harri.salokorpi@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/session/Request.scala
+++ ccf/src/main/scala/ccf/session/Request.scala
@@ -4,13 +4,22 @@ import ccf.transport.Request
 
 object JoinRequest {
   def apply(channelId: ChannelId)(s: Session): Request = Request(
-    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString, "type" -> "channel/join"),
+    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
+        "type" -> "channel/join"),
     Some(Map("channelId" -> channelId.toString))
   )
 }
 object PartRequest {
   def apply(channelId: ChannelId)(s: Session): Request = Request(
-    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString, "type" -> "channel/part"),
+    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
+        "type" -> "channel/part"),
     Some(Map("channelId" -> channelId.toString))
+  )
+}
+object OperationRequest {
+  def apply(requestType: String, channelId: ChannelId, content: Option[Any])(s: Session): Request = Request(
+    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
+        "channelId" -> channelId.toString, "type" -> requestType),
+    content
   )
 }
