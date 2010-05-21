191943bbe69beb1da75be71419957aa6788c0000
Karim Osman <karim.osman@reaktor.fi>
2010-05-21 15:15:48 +0300

    session: Remove duplicate code from Messages
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/session/Message.scala
+++ ccf/src/main/scala/ccf/session/Message.scala
@@ -6,26 +6,21 @@ case object Shutdown
 
 trait Message {
   def send(s: Session): (Session, Option[Response])
+  protected def send(s: Session, request: Request, channels: Set[ChannelId]) = {
+    val nextSession = s.next(channels)
+    val response = s.connection.send(request)
+    (nextSession, response)
+  }
 }
 
 case class Join(channelId: ChannelId) extends Message {
-  def send(s: Session): (Session, Option[Response]) = if (!s.channels(channelId)) {
-    val nextSession = s.next(s.channels + channelId)
-    val response = s.connection.send(JoinRequest(s, channelId))
-    (nextSession, response)
-  } else { (s, None) }
+  def send(s: Session): (Session, Option[Response]) = 
+    if (!s.channels(channelId)) send(s, JoinRequest(s, channelId), s.channels + channelId) else (s, None)
 }
 case class Part(channelId: ChannelId) extends Message {
-  def send(s: Session): (Session, Option[Response]) = if (s.channels(channelId)) {
-    val nextSession = s.next(s.channels - channelId)
-    val response = s.connection.send(PartRequest(s, channelId))
-    (nextSession, response)
-  } else { (s, None) }
+  def send(s: Session): (Session, Option[Response]) = 
+    if (s.channels(channelId)) send(s, PartRequest(s, channelId), s.channels - channelId) else (s, None)
 }
 case class InChannelMessage(requestType: String, channelId: ChannelId, content: Option[Any]) extends Message {
-  override def send(s: Session): (Session, Option[Response]) = {
-    val nextSession = s.next(s.channels)
-    val response = s.connection.send(InChannelRequest(s, requestType, channelId, content))
-    (nextSession, response)
-  }
+  def send(s: Session): (Session, Option[Response]) = send(s, InChannelRequest(s, requestType, channelId, content), s.channels)
 }
