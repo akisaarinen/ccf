5dbb5c3cce88b0855e7b739edee32c926509fb8a
Karim Osman <karim.osman@reaktor.fi>
2010-05-21 13:56:35 +0300

    session: Remove duplicate code from Requests
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- ccf/src/main/scala/ccf/session/Request.scala
+++ ccf/src/main/scala/ccf/session/Request.scala
@@ -2,24 +2,28 @@ package ccf.session
 
 import ccf.transport.Request
 
-object JoinRequest {
-  def apply(channelId: ChannelId)(s: Session): Request = Request(
-    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
-        "type" -> "channel/join"),
-    Some(Map("channelId" -> channelId.toString))
-  )
-}
-object PartRequest {
-  def apply(channelId: ChannelId)(s: Session): Request = Request(
-    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
-        "type" -> "channel/part"),
-    Some(Map("channelId" -> channelId.toString))
-  )
-}
-object InChannelRequest {
-  def apply(requestType: String, channelId: ChannelId, content: Option[Any])(s: Session): Request = Request(
+abstract class AbstractRequest {
+  protected def request(s: Session, requestType: String, channelId: ChannelId, content: Option[Any]): Request = Request(
     Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
         "channelId" -> channelId.toString, "type" -> requestType),
     content
   )
+}
+
+abstract class SessionControlRequest extends AbstractRequest {
+  protected def request(s: Session, requestType: String, channelId: ChannelId): Request = request(s, requestType, channelId, content(channelId))
+  private def content(channelId: ChannelId) = Some(Map("channelId" -> channelId.toString))
+}
+
+object JoinRequest extends SessionControlRequest {
+  def apply(channelId: ChannelId)(s: Session): Request = request(s, "channel/join", channelId)
+}
+
+object PartRequest extends SessionControlRequest  {
+  def apply(channelId: ChannelId)(s: Session): Request = request(s, "channel/part", channelId)
+}
+
+object InChannelRequest extends AbstractRequest {
+  def apply(requestType: String, channelId: ChannelId, content: Option[Any])(s: Session): Request = 
+    request(s, requestType, channelId, content)
 }
