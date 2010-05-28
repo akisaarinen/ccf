5aa8f2218ba0868ea5cd1b7b71a9f318e44e0f09
Karim Osman <karim.osman@reaktor.fi>
2010-05-28 17:07:09 +0300

    tree: Add #encode to Modifier
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Acked-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- ccf/src/main/scala/ccf/tree/operation/Modifier.scala
+++ ccf/src/main/scala/ccf/tree/operation/Modifier.scala
@@ -4,9 +4,10 @@ import ccf.tree.TreeNode
 
 trait Modifier {
   def apply(node: TreeNode)
+  def encode: Any
 }
 
 case class NopModifier() extends Modifier {
   def apply(node: TreeNode) {}
+  def encode: Any = "nop"
 }
-
