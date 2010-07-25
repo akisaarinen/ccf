4ee028845dc3394d51cfe075a483025edd6bd3c2
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-07-25 17:35:30 +0300

    session: Reformat code and introduce variable for clarity
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- ccf/src/main/scala/ccf/session/SessionActor.scala
+++ ccf/src/main/scala/ccf/session/SessionActor.scala
@@ -9,7 +9,13 @@ class SessionActor(connection: Connection, clientId: ClientId, version: Version,
     this(connection, clientId, version, Session(connection, version, clientId, 0, Set()))
   }
   def act { loop(session) }
-  private def loop(s: Session) { react { case msg: Message => loop(handleMessage(s, msg)); case Shutdown => exit } }
+  private def loop(s: Session) { react {
+    case msg: Message => {
+      val nextSession = handleMessage(s, msg)
+      loop(nextSession)
+    }
+    case Shutdown => exit
+  }}
   private def handleMessage(session: Session, msg: Message): Session = session.send(msg) match {
     case (nextSession: Session, result: Either[Failure, Success]) => { sender ! result; nextSession }
   }
