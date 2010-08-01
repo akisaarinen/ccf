17ecde197d30bbd9db5c74d8c9c437933a12ad09
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-08-01 17:25:11 +0300

    operation: Implement encoding of OperationContext
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- ccf/src/main/scala/ccf/OperationContext.scala
+++ ccf/src/main/scala/ccf/OperationContext.scala
@@ -3,5 +3,5 @@ package ccf
 import ccf.operation.Operation
 
 case class OperationContext[T <: Operation](val op: T, val localMsgSeqNo: Int, val remoteMsgSeqNo: Int) {
-  def encode: Any = error("not implemented")
+  def encode: Any = Map("op" -> op.encode, "localMsgSeqNo" -> localMsgSeqNo, "remoteMsgSeqNo" -> remoteMsgSeqNo)
 }
