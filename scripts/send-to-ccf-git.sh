23defe256bf198a662d0ff01be92a4732c6d2065
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-08-04 20:42:39 +0300

    scripts: Add possibility to use proxied SMTP connection
    With --proxy localhost:7777 is now used as smtp server. This allows
    easier proxying of connections when Reaktor smtp server is not
    available.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- scripts/send-to-ccf-git.sh
+++ scripts/send-to-ccf-git.sh
@@ -2,5 +2,13 @@
 SMTP_SERVER=smtp.reaktor.fi
 SMTP_SERVER_PORT=25
 MAIL_ADDR=ccf-git@reaktor.fi
+
+if [ $1 == '--proxy' ] 
+then
+    SMTP_SERVER=localhost
+    SMTP_SERVER_PORT=7777
+    shift
+fi
+
 git send-email --suppress-from --smtp-server=$SMTP_SERVER --smtp-server-port=$SMTP_SERVER_PORT --to=$MAIL_ADDR $1
 
