8651d2813dbebf47ce260158d0b1b49ab318ca38
Aki Saarinen <aki.saarinen@reaktor.fi>
2010-08-04 20:40:24 +0300

    scripts: Update patch sending script name
    We're currently mailing to ccf list, no longer to the old backlog list.
    Remove references to that.
    
    Signed-off-by: Aki Saarinen <aki.saarinen@reaktor.fi>

--- scripts/send-to-ccf-git.sh
+++ scripts/send-to-ccf-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SMTP_SERVER=smtp.core.reaktor.fi
+SMTP_SERVER=smtp.reaktor.fi
 SMTP_SERVER_PORT=25
-MAIL_ADDR=backlog-git@reaktor.fi
+MAIL_ADDR=ccf-git@reaktor.fi
 git send-email --suppress-from --smtp-server=$SMTP_SERVER --smtp-server-port=$SMTP_SERVER_PORT --to=$MAIL_ADDR $1
 
