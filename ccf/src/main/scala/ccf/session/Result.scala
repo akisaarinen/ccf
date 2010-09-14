b900a9333a00f8b6191213340cfdce16b4d9b5ae
Sami Rosendahl <sami.rosendahl@reaktor.fi>
2010-09-14 14:14:12 +0300

    session: Change Success and Failure Message attribute to type Any
    Also renamed the attribute from message => request. No client or test code
    referred to the attribute.
    
    Change from Message to Any type makes the result classes more reusable within
    the session package.
    
    The change compiles and passes tests cleanly as test code and cliets are only
    interested about the identity of the Message object and not its properties.
    
    Signed-off-by: Timo Mali <timo.mali@reaktor.fi>
    Signed-off-by: Sami Rosendahl <sami.rosendahl@reaktor.fi>

--- ccf/src/main/scala/ccf/session/Result.scala
+++ ccf/src/main/scala/ccf/session/Result.scala
@@ -16,5 +16,5 @@
 
 package ccf.session
 
-case class Success(message: Message, result: Option[Any])
-case class Failure(message: Message, reason: String)
+case class Success(request: Any, result: Option[Any])
+case class Failure(request: Any, reason: String)
