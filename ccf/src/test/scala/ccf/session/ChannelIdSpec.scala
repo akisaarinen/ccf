4e529aa550ad4c3d9c3be7b978bd1e3c90eac2c7
Nuutti Rintala <nuutti.rintala@reaktor.fi>
2010-09-21 14:07:37 +0300

    session: Rename spec variable clientId -> channelId
    Signed-off-by: Nuutti Rintala <nuutti.rintala@reaktor.fi>

--- ccf/src/test/scala/ccf/session/ChannelIdSpec.scala
+++ ccf/src/test/scala/ccf/session/ChannelIdSpec.scala
@@ -20,9 +20,9 @@ import org.specs.Specification
 
 object ChannelIdSpec extends Specification {
   "Random ChannelId" should {
-    val clientId = ChannelId.randomId
+    val channelId = ChannelId.randomId
     "not be equal to another random ChannelId" in {
-      clientId must not equalTo(ChannelId.randomId)
+      channelId must not equalTo(ChannelId.randomId)
     }
   }
 
