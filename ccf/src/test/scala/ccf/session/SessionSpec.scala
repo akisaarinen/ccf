02684d34f883706dc33345a3a21277cc40202163
Sami Rosendahl <sami.rosendahl@reaktor.fi>
2010-09-28 09:36:10 +0300

    session: Refactor SessionSpec structure and spec naming
    Signed-off-by: Sami Rosendahl <sami.rosendahl@reaktor.fi>

--- ccf/src/test/scala/ccf/session/SessionSpec.scala
+++ ccf/src/test/scala/ccf/session/SessionSpec.scala
@@ -31,124 +31,116 @@ object SessionSpec extends Specification with Mockito {
   val session = Session(connection, version, clientId, 0, channels)
   val testResult = Some("test result")
   val testReason = "test reason"
-  "Session next(...)" should {
+
+  "Session#next" should {
     "produce new session with incremented seqId" in {
       session.next(Set()).seqId must equalTo(1)
     }
+    
     "produce new session with updated channel list" in {
       session.next(Set(newChannelId)).channels must equalTo(Set(newChannelId))
     }
   }
-  "Session send(...)" should {
-    "with any message" in {
-      val msg = mock[Message]
-      val mockReq = mock[SessionRequest]
-      val mockRes = mock[SessionResponse]
-      val newSession = session.copy(seqId = session.seqId+1)
 
-      "Return success and new session on empty response" in {
-        msg.send(session) returns((newSession, None))
+  "Session#send with any message" should {
+    val msg = mock[Message]
+    val mockReq = mock[SessionRequest]
+    val mockRes = mock[SessionResponse]
+    val newSession = session.copy(seqId = session.seqId+1)
 
-        val (nextSession, result) = session.send(msg)
-        there was one(msg).send(session)
-        result must equalTo(Right(Success(msg, None)))
-        nextSession must equalTo(newSession)
-      }
-      "Return success and new session on success response without result" in {
-        mockRes.result returns Left(Success(mockReq, None))
-        msg.send(session) returns((newSession, Some(mockRes)))
+    "return success and new session on empty response" in {
+      msg.send(session) returns((newSession, None))
 
-        val (nextSession, result) = session.send(msg)
-        there was one(msg).send(session)
-        result must equalTo(Right(Success(msg, None)))
-        nextSession must equalTo(newSession)
-      }
-      "Return success and new session on success response with result" in {
-        mockRes.result returns Left(Success(mockReq, testResult))
-        msg.send(session) returns((newSession, Some(mockRes)))
-
-        val (nextSession, result) = session.send(msg)
-        there was one(msg).send(session)
-        result must equalTo(Right(Success(msg, testResult)))
-        nextSession must equalTo(newSession)
-      }
-      "Return failure and retain session state on failure response" in {
-        mockRes.result returns Right(Failure(mockReq, testReason))
-        msg.send(session) returns((newSession, Some(mockRes)))
-
-        val (nextSession, result) = session.send(msg)
-        there was one(msg).send(session)
-        result must equalTo(Left(Failure(msg, testReason)))
-        nextSession must equalTo(session)
-      }
-      "Return failure and retain session state on exception in Message#send" in {
-        val ex = new RuntimeException(testReason)
-        msg.send(session) throws ex
-
-        val (nextSession, result) = session.send(msg)
-        there was one(msg).send(session)
-        result must equalTo(Left(Failure(msg, ex.toString)))
-        nextSession must equalTo(session)
-      }
+      val (nextSession, result) = session.send(msg)
+      there was one(msg).send(session)
+      result must equalTo(Right(Success(msg, None)))
+      nextSession must equalTo(newSession)
     }
-    "with Join message" in {
-      val joinRequest = JoinRequest(session, newChannelId)
-      val joinMessage = Join(newChannelId)
 
-      "send valid request, sending correct TransportRequest and producing correct Session" in {
-        connection.send(joinRequest.transportRequest) returns None
-        val (nextSession, result) = session.send(joinMessage)
+    "return success and new session on success response without result" in {
+      mockRes.result returns Left(Success(mockReq, None))
+      msg.send(session) returns((newSession, Some(mockRes)))
 
-        result must equalTo(Right(Success(joinMessage, None)))
-        nextSession.seqId must equalTo(1)
-        nextSession.channels must equalTo(Set(existingChannelId, newChannelId))
-        there was one(connection).send(joinRequest.transportRequest)
-      }
+      val (nextSession, result) = session.send(msg)
+      there was one(msg).send(session)
+      result must equalTo(Right(Success(msg, None)))
+      nextSession must equalTo(newSession)
     }
-    "with Part message" in {
-      val partRequest = PartRequest(session, existingChannelId)
-      val partMessage = Part(existingChannelId)
 
-      "send valid request, sending correct TransportRequest and producing correct Session" in {
-        connection.send(partRequest.transportRequest) returns None
-        val (nextSession, result) = session.send(partMessage)
+    "return success and new session on success response with result" in {
+      mockRes.result returns Left(Success(mockReq, testResult))
+      msg.send(session) returns((newSession, Some(mockRes)))
 
-        result must equalTo(Right(Success(partMessage, None)))
-        nextSession.seqId must equalTo(1)
-        nextSession.channels must equalTo(Set())
-        there was one(connection).send(partRequest.transportRequest)
-      }
+      val (nextSession, result) = session.send(msg)
+      there was one(msg).send(session)
+      result must equalTo(Right(Success(msg, testResult)))
+      nextSession must equalTo(newSession)
     }
-    "with InChannel message" in {
-      val content = Some("test content")
-      val requestType = "test/type"
-      val inChannelRequest = InChannelRequest(session, requestType, existingChannelId, content)
-      val inChannelMessage = InChannelMessage(requestType, existingChannelId, content)
 
-      "send valid request, sending correct TransportRequest and producing correct Session" in {
-        connection.send(inChannelRequest.transportRequest) returns None
-        val (nextSession, result) = session.send(inChannelMessage)
+    "return failure and retain session state on failure response" in {
+      mockRes.result returns Right(Failure(mockReq, testReason))
+      msg.send(session) returns((newSession, Some(mockRes)))
 
-        result must equalTo(Right(Success(inChannelMessage, None)))
-        nextSession.seqId must equalTo(1)
-        nextSession.channels must equalTo(Set(existingChannelId))
-        there was one(connection).send(inChannelRequest.transportRequest)
-      }
+      val (nextSession, result) = session.send(msg)
+      there was one(msg).send(session)
+      result must equalTo(Left(Failure(msg, testReason)))
+      nextSession must equalTo(session)
     }
-    "with OperationContextMessage message" in {
-      val context = mock[OperationContext]
-      val operationContextRequest = OperationContextRequest(session, existingChannelId, context)
-      val operationContextMessage = OperationContextMessage(existingChannelId, context)
 
-      "send valid request, sending correct TransportRequest and producing correct Session" in {
-        connection.send(operationContextRequest.transportRequest) returns None
-        val (nextSession, result) = session.send(operationContextMessage)
+    "return failure and retain session state on exception in Message#send" in {
+      val ex = new RuntimeException(testReason)
+      msg.send(session) throws ex
 
-        result must equalTo(Right(Success(operationContextMessage, None)))
-        nextSession.seqId must equalTo(1)
-        nextSession.channels must equalTo(Set(existingChannelId))
-        there was one(connection).send(operationContextRequest.transportRequest)
-      }
+      val (nextSession, result) = session.send(msg)
+      there was one(msg).send(session)
+      result must equalTo(Left(Failure(msg, ex.toString)))
+      nextSession must equalTo(session)
+    }
+  }
+
+  "Session#send with join message" should {
+    val joinRequest = JoinRequest(session, newChannelId)
+    val joinMessage = Join(newChannelId)
+    val expectedSession = session.next(session.channels + newChannelId)
+
+    sendMessageSpec(joinMessage, joinRequest, expectedSession)
+  }
+
+  "Session#send with part message" should {
+    val partRequest = PartRequest(session, existingChannelId)
+    val partMessage = Part(existingChannelId)
+    val expectedSession = session.next(session.channels - existingChannelId)
+
+    sendMessageSpec(partMessage, partRequest, expectedSession)
+  }
+
+  "Session#send with in-channel message" should {
+    val content = Some("test content")
+    val requestType = "test/type"
+    val inChannelRequest = InChannelRequest(session, requestType, existingChannelId, content)
+    val inChannelMessage = InChannelMessage(requestType, existingChannelId, content)
+    val expectedSession = session.next(session.channels)
+
+    sendMessageSpec(inChannelMessage, inChannelRequest, expectedSession)
+  }
+
+  "Session#send with operation context message" should {
+    val context = mock[OperationContext]
+    val operationContextRequest = OperationContextRequest(session, existingChannelId, context)
+    val operationContextMessage = OperationContextMessage(existingChannelId, context)
+    val expectedSession = session.next(session.channels)
+
+    sendMessageSpec(operationContextMessage, operationContextRequest, expectedSession)
+  }
+
+  def sendMessageSpec(msg: Message, expectedRequest: SessionRequest, expectedSession: Session) {
+    "send correct request and produce correct session" in {
+      connection.send(expectedRequest.transportRequest) returns None
+      val (nextSession, result) = session.send(msg)
+
+      there was one(connection).send(expectedRequest.transportRequest)
+      result must equalTo(Right(Success(msg, None)))
+      nextSession must equalTo(expectedSession)
     }
   }
 }
