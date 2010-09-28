c58b021964c66ff317b8d78b35c565ac3c746150
Sami Rosendahl <sami.rosendahl@reaktor.fi>
2010-09-28 16:17:35 +0300

    session: Add symbolic constants for session request attributes
    Signed-off-by: Sami Rosendahl <sami.rosendahl@reaktor.fi>

--- ccf/src/main/scala/ccf/session/SessionRequestFactory.scala
+++ ccf/src/main/scala/ccf/session/SessionRequestFactory.scala
@@ -19,20 +19,25 @@ package ccf.session
 import ccf.transport.TransportRequest
 
 object SessionRequestFactory {
+  val SequenceIdKey = "sequenceId"
+  val VersionKey = "version"
+  val ClientIdKey = "clientId"
+  val ChannelIdKey = "channelId"
+  val TypeKey = "type"
   val JoinType = "channel/join"
   val PartType = "channel/part"
   val SessionControlTypes = Set(JoinType, PartType)
   val OperationContextType = "channel/context"
 
   def transportRequestHeaders(s: Session, requestType: String, channelId: ChannelId): Map[String, String] =
-    Map("sequenceId" -> s.seqId.toString, "version" -> s.version.toString, "clientId" -> s.clientId.id.toString,
-        "channelId" -> channelId.toString, "type" -> requestType)
+    Map(SequenceIdKey -> s.seqId.toString, VersionKey -> s.version.toString, ClientIdKey -> s.clientId.id.toString,
+        ChannelIdKey -> channelId.toString, TypeKey -> requestType)
 
   def transportRequest(s: Session, requestType: String, channelId: ChannelId, content: Option[Any]): TransportRequest =
     TransportRequest(transportRequestHeaders(s, requestType, channelId), content)
 
   private[session] def sessionRequest(transportRequest: TransportRequest): SessionRequest = {
-    transportRequest.header("type") match {
+    transportRequest.header(TypeKey) match {
       case Some(JoinType) => JoinRequest(transportRequest)
       case Some(PartType) => PartRequest(transportRequest)
       case Some(OperationContextType) => OperationContextRequest(transportRequest)
